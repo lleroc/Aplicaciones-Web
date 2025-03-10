ebd42c244a70721b009d9da3837e84a04272d359
lleroc <llleroc@yahoo.es>
2025-03-10 18:47:49 -0500

    semana5-roles

---
+++ Semana 5/Roles_Estructuras_Control/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Roles_Estructuras_Control.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
