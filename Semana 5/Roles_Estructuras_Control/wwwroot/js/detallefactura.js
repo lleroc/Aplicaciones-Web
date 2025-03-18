b37cd43a0fdbbc6cd8b7d15f0d0d101f0f4d2151
lleroc <llleroc@yahoo.es>
2025-03-17 20:37:11 -0500

    detalle_factura

--- Semana 5/Roles_Estructuras_Control/wwwroot/js/detallefactura.js
+++ Semana 5/Roles_Estructuras_Control/wwwroot/js/detallefactura.js
@@ -6,7 +6,7 @@
     listaClientes() {
         var html = "<option value=0>Seleccione un opcion</option>"
         $.get("../../clientes/ListaClientes", (listaclientes) => {
-            console.log(listaclientes)
+            
             $.each(listaclientes, (index, valor) => {
                 html += `<option value=${valor.id}>${valor.nombre}</option>` 
             })
@@ -14,4 +14,41 @@
         })
     }
 
+    unCliente(id) {
+        $.get("../../clientes/unCliente?id="+id, (cliente) => {
+            console.log(cliente)
+            $("#NombreCliente").val(cliente.nombre)                        
+            $("#DireccionCliente").val(cliente.direccion)                        
+            $("#TelefonoCliente").val(cliente.telefono)                        
+            $("#EmailCliente").val(cliente.email)                        
+        })
+    }
+    nuevoCliente() {
+        $("#NombreCliente").prop('disabled', false)
+        $("#DireccionCliente").prop('disabled', false)
+        $("#TelefonoCliente").prop('disabled', false)
+        $("#EmailCliente").prop('disabled', false)
+        $("#listaClientes").prop('disabled', true)
+        $("#nuevoCliente").val(1)
+        $("#cancelar").css('display', "block")
+    }
+
+    listaProductos() {
+
+    }
+
+
+
+
+
+
+    limpiarCampos() {
+        $("#NombreCliente").prop('disabled', true)
+        $("#DireccionCliente").prop('disabled', true)
+        $("#TelefonoCliente").prop('disabled', true)
+        $("#EmailCliente").prop('disabled', true)
+        $("#listaClientes").prop('disabled', false)
+        $("#nuevoCliente").val(0)
+        $("#cancelar").css('display', "none")
+    }
 }
--- Semana 5/Roles_Estructuras_Control/wwwroot/js/site.js
+++ Semana 5/Roles_Estructuras_Control/wwwroot/js/site.js
@@ -10,3 +10,19 @@ var detallefactura = () => {
     var leerClientes = new DetalleFactura()
     leerClientes.listaClientes()
 }
+
+var unCliente = () => {
+    var id = $('#listaClientes').val()
+    var uncliente = new DetalleFactura()
+    uncliente.unCliente(id)
+}
+
+var nuevoCliente = () => {
+    var nuevoCliente = new DetalleFactura()
+    nuevoCliente.nuevoCliente()
+}
+
+var limpiarcajas = () => {
+    var limpiarcajas = new DetalleFactura()
+    limpiarcajas.limpiarCampos()
+}
