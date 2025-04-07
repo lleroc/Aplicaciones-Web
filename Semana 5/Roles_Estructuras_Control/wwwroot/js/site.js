d38a8b4794d7b2f557ca6aa4984d07a6c45d8c5d
lleroc <llleroc@yahoo.es>
2025-04-07 18:59:33 -0500

    jquery DataTables

--- Semana 5/Roles_Estructuras_Control/wwwroot/js/site.js
+++ Semana 5/Roles_Estructuras_Control/wwwroot/js/site.js
@@ -2,6 +2,11 @@
 
     () => {
         detallefactura()
+        $('#myTable').DataTable({
+            "language": {
+                "url": "https://cdn.datatables.net/plug-ins/2.2.2/i18n/es-ES.json"
+            }
+        });
     }
 
 );
